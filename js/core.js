433de9066135bb4c83cbda69e063fd41559acb6b
gprva <148754074+gprva@users.noreply.github.com>
2024-02-22 20:08:31 +0600

    first commit

---
+++ js/core.js
@@ -0,0 +1,108 @@
+// // //1
+// // function cvadrat(number){
+// //     return number * number
+// // }
+// // console.log(cvadrat(8))
+
+// // //2
+// // function summ(a,b){
+// //     return a + b
+// // }
+// // console.log(summ(4,9))
+
+// // //3
+// // function lengthStroke(str){
+// //     return str.length
+// // }
+// // console.log(lengthStroke("gaparova "))
+
+// // //4
+// // function masif(arr){
+// //     return arr[0]
+// // }
+// // console.log(masif([]))
+
+// // //5
+// // function boolion(arr5){
+// //     return !arr5
+// // }
+// // console.log(boolion(true))
+
+// //6
+// // function arifmetica(numbers){
+// //     if (numbers.length === 0){
+// //         return null;
+// //     }
+// //     let sum = 0;
+// //     for (let num of numbers){
+// //         sum += num
+// //     }
+// //     return sum / numbers.length;
+// // }
+// // let numbers = [1,2,5,7,9,1]
+// // console.log(arifmetica(numbers))
+
+// //7
+// function num7(numReverse){
+//     return numReverse.split('').reverse().join('');
+// }
+// console.log(num7 ("qwerty") )
+
+
+
+// //8
+
+
+// //10
+// function findComElements(arr1,arr2){
+//     const comElements = 
+//     arr1.filter(element => 
+//     arr2.includes(element))
+//     return comElements
+// }
+// arr1 = [1,2,3,4,5];
+// arr2 = [3,4,5,6,7];
+// console.log(findComElements(arr1,arr2))
+
+// //11
+
+
+
+// проверка себя
+
+
+//1.Напишите функцию, которая принимает число в качестве аргумента и возвращает его квадрат.
+function cvadrat(number){
+    return number**2
+}
+console.log(cvadrat(5))
+
+
+// 2.Напишите функцию, которая принимает два числа в качестве аргументов и возвращает их сумму.
+function summa(number2,number3){
+    return number2 + number3
+}console.log(summa(5,4))
+
+// 3.Напишите функцию, которая принимает строку в качестве аргумента и возвращает ее длину.
+function length(stroke){
+    return stroke.length
+}
+console.log(length("123456"))
+
+// 4.Напишите функцию, которая принимает массив в качестве аргумента и возвращает первый элемент массива.
+function firstElement(stroke2){
+    return stroke2[0]
+}console.log(firstElement("Gaparova"))
+
+// 5.Напишите функцию, которая принимает логическое значение (true или false) в качестве аргумента и возвращает противоположное значение.
+function bolion(stroke3){
+    return ! stroke3
+}
+console.log(bolion(true))
+
+//7.Напишите функцию, которая принимает строку в качестве аргумента и возвращает ее в обратном порядке.
+function reversee(stroke4){
+    return stroke4.split('').reverse().join('');
+}
+console.log(reversee ("gaparova"))
+
