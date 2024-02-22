3fe173831a05f72f9b2c755ba3a8f1751f837d32
gprva <148754074+gprva@users.noreply.github.com>
2024-02-22 20:13:39 +0600

    deleted the wrong task

--- js/core.js
+++ js/core.js
@@ -89,12 +89,6 @@ function length(stroke){
 }
 console.log(length("123456"))
 
-// 3.Напишите функцию, которая принимает строку в качестве аргумента и возвращает ее длину.
-function length(stroke){
-    return stroke.length
-}
-console.log(length("123456"))
-
 // 4.Напишите функцию, которая принимает массив в качестве аргумента и возвращает первый элемент массива.
 function firstElement(stroke2){
     return stroke2[0]
